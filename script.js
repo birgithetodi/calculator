a0e97c763b2d0710effdbd3928004dfd3c3e1a14
Birgithe T. <Birgithe.todi@tptlive.ee>
2024-12-12 15:11:49 +0200

    calculator

--- script.js
+++ script.js
@@ -1,27 +1,48 @@
+const url = "https://tink.fly.dev/chat/MBtheGoat"
 let input = document.getElementById('inputBox');
 let buttons = document.querySelectorAll('button');
 
-let string = "";
+let tehe = "";
 let arr = Array.from(buttons);
 arr.forEach(button => {
-    button.addEventListener('click', (e) =>{
-        if(e.target.innerHTML == '='){
-            string = eval(string);
-            input.value = string;
+    button.addEventListener('click', (e) => {
+        if (e.target.innerHTML == '=') {
+            answer = eval(tehe);
+            input.value = answer;
+
+            fetch(url, {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({tehe, answer})
+            });
+
         }
 
-        else if(e.target.innerHTML == 'AC'){
-            string = "";
-            input.value = string;
+        else if (e.target.innerHTML == 'AC') {
+            tehe = "";
+            input.value = tehe;
         }
-        else if(e.target.innerHTML == 'DEL'){
-            string = string.substring(0, string.length-1);
-            input.value = string;
+        else if (e.target.innerHTML == 'DEL') {
+            tehe = tehe.substring(0, tehe.length - 1);
+            input.value = tehe;
         }
-        else{
-            string += e.target.innerHTML;
-            input.value = string;
+        else {
+            tehe += e.target.innerHTML;
+            input.value = tehe;
         }
-        
+
     })
 })
+
+
+
+async function fetchMessages() {
+    const response = await fetch(url);
+    const data = await response.json();
+    const element = document.querySelector(".history");
+    element.innerHTML = 'History'
+    console.log("tere")
+    data.forEach(item => element.innerHTML += `<p>${item.tehe + " = " + item.answer}</p>`)
+}
+
+setInterval(fetchMessages, 3000);
